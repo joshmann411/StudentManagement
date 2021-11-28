8fa6158d15432fd3f91bccdad158c27425ac7696
LAPTOP-1M73F46U\JO <joshman411@gmail.com>
2021-11-29 00:59:46 +0200

    + lagging files push

---
+++ StudentMngt/StudentMngt/Migrations/20211128225541_ImproveNamingConvention.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StudentMngt.Migrations
+{
+    public partial class ImproveNamingConvention : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Student",
+                table: "Student");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Enrollment",
+                table: "Enrollment");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ContactType",
+                table: "ContactType");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Contact",
+                table: "Contact");
+
+            migrationBuilder.RenameTable(
+                name: "Student",
+                newName: "Students");
+
+            migrationBuilder.RenameTable(
+                name: "Enrollment",
+                newName: "Enrollments");
+
+            migrationBuilder.RenameTable(
+                name: "ContactType",
+                newName: "ContactTypes");
+
+            migrationBuilder.RenameTable(
+                name: "Contact",
+                newName: "Contacts");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Students",
+                table: "Students",
+                column: "StudentId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Enrollments",
+                table: "Enrollments",
+                column: "EnrollmentId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ContactTypes",
+                table: "ContactTypes",
+                column: "ContactTypeId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Contacts",
+                table: "Contacts",
+                column: "ContactId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Students",
+                table: "Students");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Enrollments",
+                table: "Enrollments");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ContactTypes",
+                table: "ContactTypes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Contacts",
+                table: "Contacts");
+
+            migrationBuilder.RenameTable(
+                name: "Students",
+                newName: "Student");
+
+            migrationBuilder.RenameTable(
+                name: "Enrollments",
+                newName: "Enrollment");
+
+            migrationBuilder.RenameTable(
+                name: "ContactTypes",
+                newName: "ContactType");
+
+            migrationBuilder.RenameTable(
+                name: "Contacts",
+                newName: "Contact");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Student",
+                table: "Student",
+                column: "StudentId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Enrollment",
+                table: "Enrollment",
+                column: "EnrollmentId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ContactType",
+                table: "ContactType",
+                column: "ContactTypeId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Contact",
+                table: "Contact",
+                column: "ContactId");
+        }
+    }
+}
