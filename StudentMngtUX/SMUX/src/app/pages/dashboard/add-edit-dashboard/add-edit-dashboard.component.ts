18e4d61482eb3184ef41d0a37d3c35cdb7ba6616
LAPTOP-1M73F46U\JO <joshman411@gmail.com>
2021-11-29 12:00:52 +0200

    + added more components needed for the implementation

---
+++ StudentMngtUX/SMUX/src/app/pages/dashboard/add-edit-dashboard/add-edit-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-edit-dashboard',
+  templateUrl: './add-edit-dashboard.component.html',
+  styleUrls: ['./add-edit-dashboard.component.css']
+})
+export class AddEditDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ StudentMngtUX/SMUX/src/app/pages/dashboard/show-dashboard/show-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-dashboard',
+  templateUrl: './show-dashboard.component.html',
+  styleUrls: ['./show-dashboard.component.css']
+})
+export class ShowDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
